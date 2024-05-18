2464bd30084b9d1a820f5ef8fed0eb56c41888bf
Bartu Sarı <167659686+bartusari@users.noreply.github.com>
2024-05-18 21:04:32 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Hesap hesap = new Hesap("13904997574", "bartu", "sarı", "5468665856", "05", "08", "2004", "123456");
+        Hesap hesap = new Hesap("11111111111", "bartu", "sarı", "05XXXXXXXXX", "05", "08", "2004", "123456");
 
         System.out.println(hesap);
         hesap.paraYatir(500);
