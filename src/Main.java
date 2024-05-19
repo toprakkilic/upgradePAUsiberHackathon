cceeb78bcad57bd9a985b4c769b005ad9729e066
Toprak KILIÇ <toprakkilic23@gmail.com>
2024-05-19 12:34:33 +0300

    bartuya push

--- src/Main.java
+++ src/Main.java
@@ -13,26 +13,33 @@ public class Main {
         do {
             String tcNumarasi;
             String sifre;
+            System.out.println("------------------------------");
             System.out.println("Upgrade bank a hoşgeldiniz!");
             System.out.println("Giriş yapmak için 1 tuşuna basınız");
             System.out.println("Kayıt olmak için 2 tuşuna basınız");
             System.out.println("Programdan çıkmak için -1 tuşlayınız");
+            System.out.println("------------------------------");
             tercih = scanner.nextInt();
 
             switch (tercih) {
                 case 1:
                     System.out.println("TC numaranizi giriniz.");
                     tcNumarasi = scanner.nextLine();
+                    System.out.println("Şifrenizi giriniz");
+                    sifre = scanner.nextLine();
                     System.out.println("Girişiniz başarılı kullanıcı işlemlerine aktariliyorsunuz.");
                 case 2:
                     tcNumarasi = scanner.nextLine();
                     System.out.println("Basariyla kayit olundu giriş yapıp işlemlerinize devam edebilirsiniz");
+                    break;
 
                 case -1:
                     System.out.println("Programdan cikis yapiliyor");
+                    break;
 
                 default:
                     System.out.println("Gecersiz bir işlem numarası girdiniz.");
+                    break;
             }
         } while (tercih != -1);
     }
@@ -42,11 +49,13 @@ public class Main {
             int secim = 0;
             do {
                 Scanner scanner = new Scanner(System.in);
+                System.out.println("------------------------------");
                 System.out.println("1- Mevcut bakiye görüntüleme");
                 System.out.println("2- Para yatırma");
                 System.out.println("3- Para çekme");
                 System.out.println("4- Para transferi");
                 System.out.println("0- Hesaptan çıkış yap");
+                System.out.println("------------------------------");
                 secim = scanner.nextInt();
 
                 switch (secim) {
@@ -70,6 +79,7 @@ public class Main {
                         
                     case 0:
                     default: System.out.println("Geçersiz bir işlem numarası girdiniz. Tekrar deneyin");
+                    break;
 
                 }
 
@@ -81,11 +91,13 @@ public class Main {
             int secim = 0;
 
             do {
+                System.out.println("------------------------------");
                 System.out.println("1- Şifre değiştirme");
                 System.out.println("2- Telefon numarası değiştirme");
                 System.out.println("3- Hesap silme");
                 System.out.println("4- Hesap bilgileri listeleme");
                 System.out.println("0- Geri dönme");
+                System.out.println("------------------------------");
                 switch (secim) {
                     case 1:
                     case 2:
@@ -103,12 +115,13 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             int secim = 0;
             do {
-
+                System.out.println("------------------------------");
                 System.out.println("1- Kredi kartı borcu görüntüleme");
                 System.out.println("2- Kredi kartı borcu ödeme");
                 System.out.println("3- Kredi kartı limit arttırma");
                 System.out.println("4- Kredi kartı başvurusu");
                 System.out.println("5-Kredi kartı şifresi değiştirme");
+                System.out.println("------------------------------");
                 secim = scanner.nextInt();
                 if(conn.selectInf("tc", TCNO).getKrediKarti() == 0){
                     System.out.println("Kredi kartınız olmadığı için kredi kartı işlemlerini kullanamazsınız.");
@@ -123,9 +136,12 @@ public class Main {
                         System.out.println("Size kredi kartı verebilmemiz için maaşınız lazım =");
                         double maas = scanner.nextDouble();
                         conn.selectInf("tc", TCNO).krediKartiBasvur(conn.selectInf("tc", TCNO), maas);
+                        break;
                     case 5:
                     case 0:
-                    default: System.out.println("Geçersiz bir işlem numarası girdiniz. Tekrar deneyin");
+                    default:
+                        System.out.println("Geçersiz bir işlem numarası girdiniz. Tekrar deneyin");
+                        break;
 
                 }
 
@@ -136,9 +152,11 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             int secim = 0;
             do {
+                System.out.println("------------------------------");
                 System.out.println("1- Kredi borcu görüntüleme");
                 System.out.println("2- Kredi başvurusu");
                 System.out.println("0- Geri dön");
+                System.out.println("------------------------------");
 
                 secim = scanner.nextInt();
 
@@ -146,7 +164,9 @@ public class Main {
                     case 1:
                     case 2:
                     case 0:
-                    default: System.out.println("Geçersiz bir işlem numarası girdiniz. Tekrar deneyin");
+                    default:
+                        System.out.println("Geçersiz bir işlem numarası girdiniz. Tekrar deneyin");
+                        break;
                 }
 
 
